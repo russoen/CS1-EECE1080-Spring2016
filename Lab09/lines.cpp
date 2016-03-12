fc14d49a430e6e1a2b63bb8d66edf04929125861
student <russoen@mail.uc.edu>
2016-03-11 21:04:37 -0500

    Filled in all functions

--- Lab09/lines.cpp
+++ Lab09/lines.cpp
@@ -33,7 +33,7 @@ class Line{
   
   // Take two points to define a line.
   Line(point_t a_temp, point_t b_temp){
-    // TODO: Fill me in
+    setPoints(a_temp, b_temp);
   }
 
   // Assign the given points to this line
@@ -44,34 +44,39 @@ class Line{
   
   // Return the first point of the line
   point_t getFirstPoint(){
-    // TODO: Fill me in
     point_t ret;
-    ret.x_coord = 0.0;
-    ret.y_coord = 0.0;
+    ret.x_coord = a.x_coord;
+    ret.y_coord = a.y_coord;
     return ret;
   }
   
   // Return the second point of the line
   point_t getSecondPoint(){
-    // TODO: Fill me in
     point_t ret;
-    ret.x_coord = 0.0;
-    ret.y_coord = 0.0;
+    ret.x_coord = b.x_coord;
+    ret.y_coord = b.y_coord;
     return ret;
   }
   
   // Given the line, what is the slope?  Return inf (constant above) if
   // the the line is vertical (no x difference).
   double getSlope(){
-    // TODO: Fill me in
-    return 0.0;
-  }
+    double slope = 0.;
+    slope = (b.y_coord - a.y_coord)/(b.x_coord - a.x_coord);
+    if((b.x_coord - a.x_coord) == 0){
+      return inf;
+    }
+      return slope;
+    }
   
   // Returns the intercept (the y location where the line intercepts the y axis).
   // If the line is vertical, return 0;
   double getIntercept(){
-    // TODO: Fill me in
-    return 0.0;
+    double intercept = a.y_coord - getSlope()*a.x_coord;
+      if(getSlope() == inf){
+        intercept = 0;
+      }
+    return intercept;
   }
   
   // Returns a string containing point1 point2
@@ -83,33 +88,43 @@ class Line{
 
   // Returns the length of the line between the two points
   double getLength(){
-    // TODO: Fill me in
-    return 0.0;
+    double length = sqrt ((b.x_coord-a.x_coord)*(b.x_coord-a.x_coord) + (b.y_coord-a.y_coord)*(b.y_coord-a.y_coord));
+    return length;
   }
   
   // Returns true if the line is horizontal  
   bool isHorizontal(){
-    // TODO: Fill me in
-    return true;
+    if(a.y_coord == b.y_coord) return true;
+    return false;
   }
   
   // Returns true if the line is vertical  
   bool isVertical(){
-    // TODO: Fill me in
-    return true;
+    if(a.x_coord == b.x_coord) return true;
+    return false;
   }
 
   // Return the distance between two lines.  If they intersect, return 0.0.
   // Return value should always be positive.
   double getDistance(Line line2){
-    // TODO: Fill me in
-    return 0.0;
+  double distance = 0.0;
+    if(abs(getSlope() - line2.getSlope()) < DDIFF){
+      distance = (fabs(getIntercept() - line2.getIntercept())/sqrt(getSlope()*getSlope() +1));
+    }else if(isVertical() && line2.isVertical()){
+      distance = fabs (a.x_coord-line2.a.x_coord); 
+    }else{
+      distance = 0.0;
+    }
+    return distance;
   }
+
 
   // Return true if the two lines are parallel.  Be sure to return the 
   // correct result if they are both vertical!
   bool isParallel(Line line2){
-    // TODO: Fill me in
+    if(getSlope() != line2.getSlope()){
+      return false;
+    }else if (getSlope() == inf && line2.getSlope() == inf) return true;
     return true;
       
   }
@@ -117,10 +132,13 @@ class Line{
   // Return true if the two lines are perpendicular.  Be sure to return the
   // correct result if they one is vertical.
   bool isPerpendicular(Line line2){
-    // TODO: Fill me in
-    return true;
+    if((getSlope() == (-1/line2.getSlope()) )|| (line2.getSlope() == (-1/getSlope())) ){
+      return true;
+    }
+    return false;
   }
 
 
 
 };
+
